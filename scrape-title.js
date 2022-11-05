6155d5c18f19857ba793af40d3759ce0c43ac1b8
Abu Taher <entrptaher@gmail.com>
2022-11-05 15:59:46 +0600

    fix: puppeteer no sandbox

--- scrape-title.js
+++ scrape-title.js
@@ -3,7 +3,9 @@ const puppeteer = require("puppeteer");
 async function scrapeTitle(url) {
   if (!url) return;
 
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
   await page.goto(url);
   const title = await page.title();
